98881675efccfa0c8929341c1f13083373185308
Daniela Zaccarello <danizaccarello@gmail.com>
2023-08-11 00:20:19 -0300

    desafio

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -13,7 +13,7 @@ export const createCart = async(req, res) => {
 
 export const getCart = async(req, res) => {
     try {
-        const cartId =req.params.cartId;
+        const cartId = req.params.cartId;
         const cart = await cartsService.getCart(cartId);
         res.json({ status: 1, cart });
     } catch(error) {
@@ -21,12 +21,56 @@ export const getCart = async(req, res) => {
     }
 }
 
-const updateCart = async (req, res) => {
+export const updateCart = async (req, res) => {
     try {
-        const cartId =req.params.cartId;
+        const cartId = req.params.cartId;
         const products = req.body.products;
         const cart = await cartsService.addToCart(cartId, products);
-        res.status(201).send({ status: 1, msg: 'Cart successfully updated', cartId: newCart._id });
+        res.status(201).send({ status: 1, msg: 'Cart successfully updated', cartProducts: cart.products });
+    } catch(error) {
+        res.status(500).send({ status: 0, msg: error.message });
+    }
+}
+
+export const addProductToCart = async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const productId = req.params.productId;
+        const cart = await cartsService.addToCart(cartId, productId);
+        res.status(201).send({ status: 1, msg: 'Product successfully added to cart', cart });
+    } catch(error) {
+        res.status(500).send({ status: 0, msg: error.message });
+    }
+}
+
+export const removeProductFromCart = async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const productId = req.params.productId;
+        const cart = await cartsService.removeFromCart(cartId, productId);
+        res.status(201).send({ status: 1, msg: 'Product successfully deleted from cart', cart });
+    } catch(error) {
+        res.status(500).send({ status: 0, msg: error.message });
+    }
+}
+
+const updateProductQuantity = async (req, res) => {
+    try {
+        const cartId = req.params.cartId;
+        const productId = req.params.productId;
+        const quantity = req.body.quantity;
+        const cart = await cartsService.updateProductQuantity(cartId, productId, quantity);
+        res.status(201).send({ status: 1, msg: 'Product successfully deleted from cart', cart });
+    } catch(error) {
+        res.status(500).send({ status: 0, msg: error.message });
+    }
+}
+
+const emptyCart = async (req, res) => {
+    const cartId = req.params.cartId;
+    try {
+        const emptyCart = await cartsService.emptyCart(cartId);
+        res.status(201).send({ status: 1, msg: 'Product successfully emptied', cart: emptyCart });
     } catch(error) {
         res.status(500).send({ status: 0, msg: error.message });
     }
